8c4b1a7a3b80f751415d1608314ef8c018a92427
messaoudisony <messaoudi.sonia89@gmail.com>
2024-03-04 14:26:58 +0100

    let-const

---
+++ index.js
@@ -0,0 +1,12 @@
+const name = "David";
+
+const lastname = "Bruno";
+
+let biographie ="Hi, I'm David Bruno from SF, I like to cook and meet new people.";
+
+let img ="http://www.go.com/davif.png";
+
+const origin= "san francisco";
+
+let followers = 109; 
+let following = 200; 
